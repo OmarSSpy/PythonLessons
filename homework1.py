62c1c7c43308881c2057d9da2db08064b32c3636
TheWEBY <thewebysf@gmail.com>
2025-02-24 17:54:09 -0300

    Python
    My first homwork with python

---
+++ homework1.py
@@ -0,0 +1,6 @@
+temp1 = 30
+tempp = int(input("What is the temperature in your room"))
+if tempp>temp1:
+    print("your room is very hot")
+else:
+    print("the temperature in your room is very beautiful")
