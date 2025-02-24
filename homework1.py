06c84d00393e4f68eabdd0dca6b6b3e42d0a5746
Omar SS <walter.seri2@gmail.com>
2025-02-24 17:55:21 -0300

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -1,6 +1,5 @@
-temp1 = 30
 tempp = int(input("What is the temperature in your room"))
-if tempp>temp1:
+if tempp>30:
     print("your room is very hot")
 else:
     print("the temperature in your room is very beautiful")
